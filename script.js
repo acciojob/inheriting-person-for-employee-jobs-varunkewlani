4afa830e18ed6464f1241d142860bcc358df450b
varunkewlani <91694985+varunkewlani@users.noreply.github.com>
2024-08-27 18:17:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 // complete this js code
-function Person(name, age) {}
+class Person{
+	constructor (name,age)
+	{
+		this.Name=name;
+		this.Age=age;
+	}
+	greet()
+	{
+		return `Hello, my name is ${this.Name}, I am ${this.Age} years old.`
+	}
+}
 
-function Employee(name, age, jobTitle) {}
+class Employee extends Person {
+	constructor(name, age, jobTitle)	
+	{
+		super(name, age)
+		this.JobTitle=jobTitle;
+	}
+	jobGreet()
+	{
+		return `Hello, my name is ${this.name}, I am ${this.Age} years old, and my job title is ${this.name}.`
+	}
+}
 
 // Do not change code below this line
 window.Person = Person;
